4b0cc4f604aaa7e23aa660b8bff0535a05daf73d
Daniel Nuñez S <dnunezse@gmail.com>
2015-06-01 15:18:12 -0300

    Se le agrega la zona horaria por defecto a UTC

--- public/index.php
+++ public/index.php
@@ -4,10 +4,16 @@
  *
  * @package     ForeverPHP
  * @author      Daniel Nuñez S. <dnunez@emarva.com>
- * @copyright   Copyright (c) 2014, Emarva.
+ * @copyright   Copyright (c) 2014-2015, Emarva.
  * @license     http://www.opensource.org/licenses/mit-license.php MIT License
  * @link        https://github.com/foreverphp/foreverphp
  */
+
+/*
+ * Se establece la zona horaria a UTC por defecto, para evitar
+ * conflictos con la función date().
+ */
+date_default_timezone_set('UTC');
 
 /*
  * Inicio la carga base
