ef80c8005286f6818c312c96bf504c370867ddec
Daniel Nuñez S <dnunezse@gmail.com>
2016-01-06 18:49:44 -0300

    Cambio por el nuevo sepador en la vista de la ruta

--- apps/routes.php
+++ apps/routes.php
@@ -2,8 +2,8 @@
 /*
  * Aca se agregan las rutas
  */
-Route::add('/', 'Main.StartView');
-Route::add('/other_method', 'Main.StartView.otherMethod');
+Route::add('/', 'Main@StartView');
+Route::add('/other_method', 'Main@StartView@otherMethod');
 Route::add('/anonymous', function() {
 	echo 'This is an anonymous function.';
 });
